a1ebdf71cb7de6d6ae87e02e4d83e068697a2ab4
Yoan Yovkov <joan414132@gmail.com>
2023-08-06 15:19:45 +0300

    a

--- Нова папка/script.js
+++ Нова папка/script.js
@@ -14,6 +14,19 @@ cityLinks.forEach(link => {
     });
 });
 
+function updateClock(city) {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById(`time-${city}`).innerText = timeString;
+}
+
+setInterval(() => updateClock('ruse'), 1000);
+setInterval(() => updateClock('sofia'), 1000);
+setInterval(() => updateClock('burgas'), 1000);
+
 
 
 
