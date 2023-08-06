2e1d1576392cf148821ce5a4ce5edd416b2dc8f8
Yoan Yovkov <joan414132@gmail.com>
2023-08-06 15:24:18 +0300

    a

--- Нова папка/script.js
+++ Нова папка/script.js
@@ -28,6 +28,30 @@ setInterval(() => updateClock('sofia'), 1000);
 setInterval(() => updateClock('burgas'), 1000);
 
 
+const cityElements = document.querySelectorAll('.clock a');
+cityElements.forEach(x=> {
+    city.style.fontSize='32px';
+});
+const clockElements = document.querySelectorAll('.clock p');
+clockElements.forEach(clock => {
+    setInterval(() => {
+        const randomColor = getRandomColor();
+        clock.style.color = randomColor; 
+    }, 1000);
+});
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+const cardElements = document.querySelectorAll('.card');
+cardElements.forEach(card => {
+    card.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)'; 
+});
 
 
 
